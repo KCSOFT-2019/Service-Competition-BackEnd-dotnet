7f973d6ccf9f137fb50b00a512e0c46205e9f66c
leoranbom <cmcho@qq.com>
2021-03-23 16:04:01 +0800

    add PermissionController

---
+++ Controllers/PerimissionController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RfidAPI.Models;
+using RfidAPI.Service;
+
+namespace RfidAPI.Controllers
+{
+    public class PerimissionController : ControllerBase
+    {
+        private readonly PermissionService _permissionService;
+
+        [HttpPost]
+        [Route("api/permission/add")]
+        public ActionResult<string> Create(Permission permission)
+        {
+            var result  = _permissionService.CreateRole(permission);
+            if (result)
+            {
+                return "permission create successfully";
+            }
+            else
+            {
+                return "permission create unsuccessfully";
+            }
+        }
+
+
+        [HttpGet]
+        [Route("api/permission/getAll")]
+        public Task<ActionResult<IEnumerable<Permission>>> Gets()
+        {
+            return _permissionService.getAllPermissions();
+        }
+    }
+}
