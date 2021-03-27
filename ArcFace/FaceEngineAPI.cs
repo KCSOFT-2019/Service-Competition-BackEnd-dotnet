facabb5fae29be34e36e706be4f6d83113375343
leoranbom <cmcho@qq.com>
2021-03-27 15:51:28 +0800

    feat(ArcFace-FaceEngineAPI,Controllers-ArcFaceController):add and test active app and EngineInitial api

--- ArcFace/FaceEngineAPI.cs
+++ ArcFace/FaceEngineAPI.cs
@@ -1,9 +1,28 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 using RfidAPI.ArcFace;
 namespace RfidAPI.ArcFace
 {
     public class FaceEngineAPI
     {
+        [DllImport("libarcsoft_face_engine.dll")]
+        public static extern long ASFGetActiveFileInfo(ArcFaceEngine.ASF_ActiveFileInfo asfActiveFileInfo);
+
+        [DllImport("libarcsoft_face_engine.dll")]
+        //public static extern long ASFOnlineActivation(IntPtr AppId, IntPtr SDKKey);
+        public static extern long ASFOnlineActivation(string AppId, string SDKKey);
+
+        [DllImport("libarcsoft_face_engine.dll")]
+        public static extern long ASFInitEngine(
+            ArcFaceEngine.ASF_DetectMode asfDetectMode,
+            ArcFaceEngine.ArcSoftFace_OrientPriority arcSoftFaceOrientPriority,
+            int detectFaceScaleVal,
+            int detectFaceMaxNum,
+            int combinedMask,
+            ref IntPtr hEngine
+        );
+        
+        
         [DllImport("libarcsoft_face_engine.dll", CharSet = CharSet.Ansi)]
         public static extern ArcFaceEngine.ASF_VERSION ASFGetVersion();
     }
--- Controllers/ArcFaceController.cs
+++ Controllers/ArcFaceController.cs
@@ -2,6 +2,7 @@
 using System.Runtime.InteropServices;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Logging;
 using RfidAPI.ArcFace;
 using RfidAPI.Models;
@@ -32,6 +33,31 @@ namespace RfidAPI.Controllers
             return ascii.GetString(asfversion.Version);*/
             
             return null;
+        }
+
+        [HttpPost("activeOnline")]
+        public ActionResult<long> ActiveOnline(string appId, string sdkkey)
+        {
+            var res = FaceEngineAPI.ASFOnlineActivation(appId, sdkkey);//FaceEngineAPI.ASFOnlineActivation(Marshal.StringToHGlobalAuto(appId), Marshal.StringToHGlobalAuto(sdkkey));
+            return res;
+        }
+
+        [HttpPost]
+        public ActionResult<long> InitEngine(
+            ArcFaceEngine.ASF_DetectMode asfDetectMode,
+            ArcFaceEngine.ArcSoftFace_OrientPriority arcSoftFaceOrientPriority,
+            int detectFaceScaleVal,
+            int detectFaceMaxNum,
+            int combinedMask)
+        {
+            IntPtr handle = IntPtr.Zero;
+            var res = FaceEngineAPI.ASFInitEngine(
+                asfDetectMode,
+                arcSoftFaceOrientPriority,
+                detectFaceScaleVal,
+                detectFaceMaxNum,
+                combinedMask, ref handle);
+            return res;
         }
     }
 }
